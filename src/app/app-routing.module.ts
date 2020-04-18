e011a7f3005fab57788e4eb1e5ca5638027da006
Anthony Ku Ong <anthonykuong@gmail.com>
2020-04-18 19:14:57 -0400

    added cast,episodes, summary modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListEpisodesComponent} from './episodes/list-episodes/list-episodes.component';
+import {SynopsisComponent} from './summary/synopsis/synopsis.component';
+import {CharactersComponent} from './cast/characters/characters.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'summary', component: SynopsisComponent},
+  {path :'cast', component: CharactersComponent},
+  {path :'list', component: ListEpisodesComponent},
+  {path :'', redirectTo: '/summary', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';  //only once
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { EpisodesModule } from './episodes/episodes.module';
+
 import { AppComponent } from './app.component';
+import {CoreModule} from './core/core.module';
+import { SummaryModule } from './summary/summary.module';
+import { CastModule } from './cast/cast.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EpisodesModule,
+    CoreModule,
+    SummaryModule,
+    CastModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
